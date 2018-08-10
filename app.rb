314cdfff94ae24b58b8ac2585b6ecc9d8e2d751f
Dominic Dagradi <dominic@chain.com>
2018-08-09 23:02:34 -0500

    sign in users!

--- app.rb
+++ app.rb
@@ -1,11 +1,22 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'sinatra/flash'
 require 'active_support/all'
 require 'nationbuilder'
 
 configure { set :server, :puma }
+enable :sessions
 
-nation_builder_client = NationBuilder::Client.new(ENV['NATION_NAME'], ENV['NATION_API_TOKEN'])
+nation_builder_client = NationBuilder::Client.new(
+  ENV['NATION_NAME'],
+  ENV['NATION_API_TOKEN']
+)
+
+helpers do
+  def signed_in_tag
+    'signed_in_meeting_beta'
+  end
+end
 
 # some options here:
 # - pull all membership at app start and do fuzzy-matches in-memory
@@ -22,6 +33,40 @@ post '/lookup' do
   @search_params[:email] = params[:email] if params[:email].present?
   @search_params[:last_name] = params[:last_name] if params[:last_name].present?
 
-  @results = nation_builder_client.call(:people, :search, @search_params)['results']
+  @results = nation_builder_client.call(
+    :people,
+    :search,
+    @search_params
+  )['results']
+
   erb :results
+end
+
+post '/signin' do
+  begin
+    person = nation_builder_client.call(
+      :people,
+      :show,
+      id: params[:id]
+    )['person']
+
+    nation_builder_client.call(
+      :people,
+      :tag_person,
+      id: params[:id],
+      tagging: {
+        tag: signed_in_tag
+      }
+    )
+
+    flash[:success] = "#{person['full_name']} has signed in. Thanks!"
+  rescue StandardError
+    flash[:error] = 'There was a problem signing in.'
+  end
+
+  redirect '/'
+end
+
+get '/new-member' do
+  erb :new_member
 end
